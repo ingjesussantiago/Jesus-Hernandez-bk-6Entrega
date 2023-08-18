8fa48d8bf86be4ba24d93f8cde383763871d78c6
Jesus Santiago Hernandez Lopez <ingjesussantiago@gmail.com>
2023-08-18 11:22:50 -0600

    carpetas de mongoose para trabajar

---
+++ src/dao/mongoosedb/dbConfig.js
@@ -0,0 +1,8 @@
+import  mongoose  from "mongoose"
+
+const URI = "mongodb+srv://ingjesussantiago:1BJqXKhkrqa9kOEM@cluster0.nwy2csb.mongodb.net/7entregabk?retryWrites=true&w=majority"
+
+
+mongoose.connect(URI)
+.then(()=>console.log("conectado a db"))
+.catch((error)=>console.log(error))
