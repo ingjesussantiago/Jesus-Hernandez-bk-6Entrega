aad13737a8d7bc31e478432e7ee69731a0c5b9c8
Jesus Santiago Hernandez Lopez <ingjesussantiago@gmail.com>
2023-08-20 14:22:48 -0600

    corrigiento nombre de basa

--- src/dao/mongoosedb/dbConfig.js
+++ src/dao/mongoosedb/dbConfig.js
@@ -1,6 +1,6 @@
 import  mongoose  from "mongoose"
 
-const URI = "mongodb+srv://ingjesussantiago:1BJqXKhkrqa9kOEM@cluster0.nwy2csb.mongodb.net/6entregabk?retryWrites=true&w=majority"
+const URI = "mongodb+srv://ingjesussantiago:1BJqXKhkrqa9kOEM@cluster0.nwy2csb.mongodb.net/6Entregabk?retryWrites=true&w=majority"
 
 
 mongoose.connect(URI)
